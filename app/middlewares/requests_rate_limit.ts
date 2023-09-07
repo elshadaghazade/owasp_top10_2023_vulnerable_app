3aa76dcf94a662df92562b7945bd283ef0dcb031
Elshad <elshadaghazade@gmail.com>
2023-09-08 01:27:08 +0400

    commit

--- app/middlewares/requests_rate_limit.ts
+++ app/middlewares/requests_rate_limit.ts
@@ -1,12 +1,21 @@
 import { NextRequest, NextResponse } from "next/server"
 
-const requestCountByIp = new Map()
+const requestCountByIp = new Map<
+  string,
+  { count: number; lastRequestTime: number; timeWindow: number }
+>()
 const maxRequests = process.env.REQUESTS_RATE_LIMIT
   ? parseInt(process.env.REQUESTS_RATE_LIMIT)
   : 1
-const timeWindow = process.env.REQUESTS_RATE_LIMIT_TIME_WINDOW ? parseInt(process.env.REQUESTS_RATE_LIMIT_TIME_WINDOW) : 3000;
+const timeWindow = process.env.REQUESTS_RATE_LIMIT_TIME_WINDOW
+  ? parseInt(process.env.REQUESTS_RATE_LIMIT_TIME_WINDOW)
+  : 3000
 
 export async function RequestsRateLimit(request: NextRequest) {
+  if (requestCountByIp.size > 1000_000) {
+    requestCountByIp.clear()
+  }
+
   const ipAddress =
     request.ip ||
     request.headers.get("x-real-ip") ||
@@ -26,13 +35,18 @@ export async function RequestsRateLimit(request: NextRequest) {
     requestCountByIp.set(ipAddress, {
       count: maxRequests,
       lastRequestTime: Date.now(),
+      timeWindow,
     })
   } else {
-    const requestData = requestCountByIp.get(ipAddress)
-    if (Date.now() - requestData.lastRequestTime >= timeWindow) {
+    const requestData = requestCountByIp.get(ipAddress)!
+    if (Date.now() - requestData.lastRequestTime >= requestData.timeWindow) {
       requestData.count = maxRequests
       requestData.lastRequestTime = Date.now()
+      requestData.timeWindow = timeWindow
     } else if (requestData.count <= 0) {
+      requestData.lastRequestTime = Date.now()
+      requestData.timeWindow += 100
+
       return NextResponse.json(
         {},
         {
