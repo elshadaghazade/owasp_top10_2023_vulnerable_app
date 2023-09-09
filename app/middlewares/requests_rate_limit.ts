cf9decc1c5102dd4c2893366953fbfc12f571e8d
Elshad <elshadaghazade@gmail.com>
2023-09-09 22:22:28 +0400

    request rate limit middleware was improved so that adding user agent and authorization token if exists to the key besides ip address. ip address can be the same for the several users if their public ip address the same

--- app/middlewares/requests_rate_limit.ts
+++ app/middlewares/requests_rate_limit.ts
@@ -11,17 +11,38 @@ const timeWindow = process.env.REQUESTS_RATE_LIMIT_TIME_WINDOW
   ? parseInt(process.env.REQUESTS_RATE_LIMIT_TIME_WINDOW)
   : 3000
 
+
+function getHashCode(request: NextRequest) {
+  const ip =
+    request.ip ||
+    request.headers.get("x-real-ip") ||
+    request.headers.get("x-forwarded-for")
+
+  if (!ip) {
+    throw new Error("Ip is wrong")
+  }
+
+  const userAgent = request.headers.get("user-agent")
+
+  if (!userAgent) {
+    throw new Error("user agent is empty")
+  }
+
+  const token = request.headers.get("Authorization") || ""
+
+  return Buffer.from(ip + userAgent + token).toString("base64")
+}
+
 export async function RequestsRateLimit(request: NextRequest) {
   if (requestCountByIp.size > 1000_000) {
     requestCountByIp.clear()
   }
 
-  const ipAddress =
-    request.ip ||
-    request.headers.get("x-real-ip") ||
-    request.headers.get("x-forwarded-for")
+  let hashCode = "";
 
-  if (!ipAddress) {
+  try {
+    hashCode = getHashCode(request)
+  } catch {
     return NextResponse.json(
       {},
       {
@@ -31,23 +52,33 @@ export async function RequestsRateLimit(request: NextRequest) {
     )
   }
 
-  if (!requestCountByIp.has(ipAddress)) {
-    requestCountByIp.set(ipAddress, {
+  if (!hashCode) {
+    return NextResponse.json(
+      {},
+      {
+        status: 400,
+        statusText: "Bad request",
+      }
+    )
+  }
+
+  if (!requestCountByIp.has(hashCode)) {
+    requestCountByIp.set(hashCode, {
       count: maxRequests,
       lastRequestTime: Date.now(),
       timeWindow,
     })
   } else {
-    const requestData = requestCountByIp.get(ipAddress)!
+    const requestData = requestCountByIp.get(hashCode)!
     if (Date.now() - requestData.lastRequestTime >= requestData.timeWindow) {
       requestData.count = maxRequests
       requestData.lastRequestTime = Date.now()
       requestData.timeWindow = timeWindow
     } else if (requestData.count <= 0) {
-      requestData.lastRequestTime = Date.now();
+      requestData.lastRequestTime = Date.now()
 
       if (requestData.timeWindow < 30000) {
-        requestData.timeWindow += 100;
+        requestData.timeWindow += 100
       }
 
       return NextResponse.json(
